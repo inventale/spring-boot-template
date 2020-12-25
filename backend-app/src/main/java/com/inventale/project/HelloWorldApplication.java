69cda78f2cf469ab865efd717a8f17625e3e6284
Vitaly Ivanov <vitaly.ivanov@maxifier.com>
2020-12-25 18:51:42 +0400

    Fix local run

--- backend-app/src/main/java/com/inventale/project/HelloWorldApplication.java
+++ backend-app/src/main/java/com/inventale/project/HelloWorldApplication.java
@@ -2,9 +2,8 @@ package com.inventale.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.inventale.project", exclude = { ContextInstanceDataAutoConfiguration.class })
+@SpringBootApplication(scanBasePackages = "com.inventale.project")
 public class HelloWorldApplication {
     public static void main(String... args) {
         SpringApplication.run(HelloWorldApplication.class, args);
---
+++ backend-app/src/main/java/com/inventale/project/LocalConfig.java
@@ -0,0 +1,16 @@
+package com.inventale.project;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@SuppressWarnings("unused")
+@Configuration
+@EnableAutoConfiguration(
+        // disable cloud auto-configurations for local profiles
+        exclude = {ContextInstanceDataAutoConfiguration.class, ContextRegionProviderAutoConfiguration.class})
+@Profile (ProfileNames.LOCAL)
+public class LocalConfig {
+}
